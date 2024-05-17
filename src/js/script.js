5285f9d77ee09cf8b506cdc9a6321e2cd23ad123
Eugene <nevin523348@gmail.com>
2024-05-17 11:41:46 +0300

    add font

--- src/js/script.js
+++ src/js/script.js
@@ -5,49 +5,25 @@ $('.review_carousel').slick({
     autoplaySpeed: 2000,
   });
 
+  const items = document.querySelectorAll('.product_wrapper_item');
 
-  document.addEventListener('DOMContentLoaded', () => {
-    // Получаем все элементы с классом product_wrapper_item
-    const items = document.querySelectorAll('.product_wrapper_item');
-
-    // Добавляем обработчики событий для каждого элемента
-    items.forEach(item => {
-        // Добавляем обработчик для наведения курсора
-        item.addEventListener('mouseenter', () => {
-            // Удаляем класс active у всех элементов с классом product_wrapper_item_footer
-            document.querySelectorAll('.product_wrapper_item_footer').forEach(footer => {
-                footer.classList.remove('active');
-            });
-
-            // Удаляем класс active у всех элементов .product_wrapper_item
-            items.forEach(item => {
-                item.classList.remove('active');
-            });
-
-            // Добавляем класс active к текущему элементу
-            item.classList.add('active');
-
-            // Показываем только footer, который является потомком текущего элемента
-            const footer = item.querySelector('.product_wrapper_item_footer');
-            if (footer) {
-                footer.classList.add('active');
-            }
-        });
-
-        // Добавляем обработчик для ухода курсора с элемента .product_wrapper_item
-        item.addEventListener('mouseleave', () => {
-            // Удаляем класс active у всех элементов с классом product_wrapper_item_footer
-            document.querySelectorAll('.product_wrapper_item_footer').forEach(footer => {
-                footer.classList.remove('active');
-            });
-
-            // Удаляем класс active у текущего элемента
-            item.classList.remove('active');
-        });
-    });
-});
+  items.forEach(item => {
+      item.addEventListener('mouseenter', () => {
+          items.forEach(otherItem => otherItem.classList.remove('active'));
+          item.classList.add('active');
+      });
+  
+      item.addEventListener('mouseleave', () => {
+          item.classList.remove('active');
+      });
+  });
+  
+  
+  
 
 
+
+//Hamburger
 document.addEventListener('DOMContentLoaded', function() {
   const burger = document.querySelector('.burger');
   const menu_laptop = document.querySelector('.menu_laptop');
